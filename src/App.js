f349f655633c6aec711764f94d13e64f5f79614c
Matias Schmidt <matias.schmidt@outlook.es>
2023-02-14 16:31:00 -0300

    Configurando rutas del front

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import Home from "./pages/Home.js"
+import Cart from "./pages/Cart.js"
+import StateBuy from "./pages/StateBuy.js"
+import NavBar from "./pages/Cart.js"
+import {Route, Routes} from "react-router-dom"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/cart" element={<Cart/>}/>
+        <Route exact path="/state" element={<StateBuy/>}/>
+      </Routes>
 
     </div>
   );
