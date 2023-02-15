715f8e3251729fa28990c92dc6c9578e8c248759
Matias Schmidt <matias.schmidt@outlook.es>
2023-02-15 12:23:39 -0300

    Agregando funcionalidad al buscador

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,40 @@
-import React from 'react';
-
+import { React, useState, useEffect} from "react";
+import {getProducts, searchProduct} from '../redux/actions.js';
+import { useDispatch, useSelector } from "react-redux";
 // import './CSS/search.css';
 
 
 
 function Search() {
+    const dispatch = useDispatch();
+    const products = useSelector((state) => state.products)
+    const [name, setName] = useState("");
+
+    const handleInputChange = (e) => {
+        e.preventDefault();
+        setName(e.target.value);
+    }
+    
+    useEffect(() => {
+        if(name.length > 1){
+            dispatch(searchProduct(name));
+            console.log("name", name)
+        } else {
+            dispatch(getProducts());
+        }
+    }, [name])
+
 
     return (
         <div className='container-search'>
-            SOY EL SEARCH
+            <div className="search">
+                <input
+                type="text"
+                value={name}
+                placeholder='Buscar productos por nombre'
+                onChange={(e) => handleInputChange(e)} 
+                />
+            </div> 
         </div>
     );
 }
