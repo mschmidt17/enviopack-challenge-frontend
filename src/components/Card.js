901e4207a1f9745d476bd4e101fc449512e4279d
Matias Schmidt <matias.schmidt@outlook.es>
2023-02-14 22:52:08 -0300

    Agregando estilos home y card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import image from "../assets/image-product.jpg";
-// import './CSS/search.css';
+import '../CSS/card.css';
 
 
 function Card({product}) {
 
     return (
         <div className='container-card'>
-            <img src={image} alt="producto" width="60"/>
+            <img src={image} alt="producto" width="160"/>
             <h3> {product.title} </h3>
             <p> ${product.price} </p>
             <button>Agregar al carrito</button>
